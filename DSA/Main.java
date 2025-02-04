b0a034a5a2813992f0b3a48c1508b60179c05fb9
PIXELATED-BRAIN <pipokinfe123456789@gmail.com>
2025-02-04 18:34:11 +0300

    //update

---
+++ DSA/Main.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("SEKIRO");
+        stack.push("ELDEN RING");
+        stack.push("SUPER HOT");
+        stack.push("FAR CRY 3");
+
+        System.out.println(stack);
+    }
+}
