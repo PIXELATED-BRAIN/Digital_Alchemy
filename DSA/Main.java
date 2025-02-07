93fe3e1db85319efa3c1b56cb787397831ce016a
PIXELATED-BRAIN <pipokinfe123456789@gmail.com>
2025-02-07 23:07:32 +0300

    //update

--- DSA/Main.java
+++ DSA/Main.java
@@ -5,11 +5,21 @@ public class Main{
         
         Stack<String> game = new Stack<String>();
 
+        //LIFO STRUCTURE
+
         game.push("SEKIRO");
         game.push("ELDEN RING");
         game.push("SUPER HOT");
         game.push("FAR CRY 3");
 
+        System.out.println(game.empty());
+
         System.out.println(game);
+
+        //String myFavGame = game.pop();
+
+        //System.out.println(myFavGame);
+        //System.out.println(game.peek());
+        //System.out.println(game.search("WITCHER"));
     }
 }
