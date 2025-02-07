b730c686211821720372787e4a2cdd9ef835176b
PIXELATED-BRAIN <pipokinfe123456789@gmail.com>
2025-02-07 23:21:42 +0300

    //update

--- WebDev/Hide & show Password/click.js
+++ WebDev/Hide & show Password/click.js
@@ -1,13 +1,12 @@
 let eyeicon = document.getElementById("eyeicon");
 let password = document.getElementById("password");
 
-eyeicon.onclick = function(){
-
-    if(password.type == "password"){
-        password.type == "text";
-        eyeicon.src = "open.jpg";
-    }else{
-        password.type == "password";
-        eyeicon.src = "spidey.jpeg";
+eyeicon.onclick = function () {
+    if (password.type === "password") {
+        password.type = "text"; // Corrected assignment
+        eyeicon.src = "img/open.png";
+    } else {
+        password.type = "password"; // Corrected assignment
+        eyeicon.src = "img/close.png";
     }
-}
+};
